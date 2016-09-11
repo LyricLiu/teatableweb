b664f900647395109cd0431ea97f43e90cbfd650
LyricLiu <yichiliu@foxmail.com>
2016-09-11 15:12:10 -0400

    edit 3 page

--- Aside-control.js
+++ Aside-control.js
@@ -1,3 +1,9 @@
+    var getTea = false;
+    var getStove = false;
+
+    var br1 = document.createElement('br');
+var br2 = document.createElement('br');
+
     var teaSetBar = document.getElementById('add');
 
     function addItem(imgSource, imgName) {
@@ -10,5 +16,28 @@
         item.appendChild(itemName);
         teaSetBar.appendChild(item);
     }
-
+    addItem('../item-pic/2.png', 'Cha Hu');
     addItem('../item-pic/1.png', 'Tea Box');
+    addItem('../item-pic/5.png', 'Pin Ming Bei');
+    addItem('../item-pic/4.png', 'Stove');
+    addItem('../item-pic/3.png', 'Cha Hai');
+   
+   
+//stove
+   var stoveAdd = document.getElementById('stove-add');
+   var A = document.createElement('a');
+   var B = document.createElement('a');
+   if(getTea == false){
+      A.setAttribute('href', './teaBox.html');
+      A.innerHTML = 'You decide to add some tea leaves.'
+      B.setAttribute('href', '');
+      B.innerHTML = 'You decide to warm the Cha Hu with boiling water.'
+      stoveAdd.appendChild(A);
+      stoveAdd.appendChild(br1);
+      stoveAdd.appendChild(br2);
+      stoveAdd.appendChild(B);
+   }else{
+      A.setAttribute('href', '');
+      A.innerHTML = 'You turn to Cha Hu to brew the tea.'
+      stoveAdd.appendChild(A);
+   }
