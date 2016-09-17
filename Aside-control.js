ce9d89b98d0aba12f4e295ab1d1543b60c51f762
LyricLiu <yichiliu@foxmail.com>
2016-09-16 21:18:11 -0400

    1finish

--- Aside-control.js
+++ Aside-control.js
@@ -3,12 +3,17 @@
     var getDesert = false;
     var getChaHu = false;
 
+    var process = [];
+
     var askForTea = document.getElementById('askForTea');
     var stove = document.getElementById('stove');
     var teaBox = document.getElementById('teaBox');
     var chaHu = document.getElementById('chaHu');
     var warmCup = document.getElementById('warmCup');
     var chaHai = document.getElementById('chaHai');
+    var pinMingBei = document.getElementById('pinMingBei');
+    var reward = document.getElementById('reward');
+    var check = document.getElementById('check');
 
 
     var teaSetBar = document.getElementById('add');
@@ -68,6 +73,7 @@ function stoveAlice(){
         }
     }
     getStove = true;
+    process.push('- Heat the water with stove');
 }
 
 function teaBoxAlice(){
@@ -76,6 +82,7 @@ function teaBoxAlice(){
     teaBox.style.display = "block";
     warmCup.style.display = "none";
     getTea = true;
+    process.push('- Get some tea leaves');
 }
 
 function chaHuAlice(){
@@ -88,9 +95,11 @@ function chaHuAlice(){
     if(getStove == false){
       chaHu1.style.display = "block";
       chaHu2.style.display = "none";
+      process.push('- Add tea leaves to Cha Hu');
     }else{
       chaHu2.style.display = "block";
       chaHu1.style.display = "none";
+      process.push('- Brew tea in Cha Hu');
     }
     getChaHu = true;
 }
@@ -99,6 +108,7 @@ function warmCupAlice(){
     stove.style.display = "none";
     askForTea.style.display = "none";
     warmCup.style.display = "block";
+    process.push('- Warm tea sets with boiling water');
 }
 
 function chaHaiAlice(){
@@ -112,9 +122,56 @@ function chaHaiAlice(){
     if(getStove == true){
       chaHai1.style.display = "block";
       chaHai2.style.display = "none";
+      if(getChaHu == true){
+        process.push('- Pour liquid into Cha Hai');
+      }else{
+        process.push('- Brew tea in Cha Hai');
+      }
     }else{
       chaHai2.style.display = "block";
       chaHai1.style.display = "none";
+      process.push('- Add tea leaves to Cha Hai');
+    }
+}
+
+function pinMingBeiAlice(){
+    chaHai.style.display = "none";
+    chaHu.style.display = "none";
+    pinMingBei.style.display = "block";
+    process.push('- Fill Pin Ming Bei with tea');
+}
+
+function rewardAlice(){
+    reward.style.display = "block";
+    pinMingBei.style.display = "none";
+}
+
+function checkAlice(){
+    reward.style.display = "none";
+    pinMingBei.style.display = "none";
+    check.style.display = "block";
+    var yourProcess = document.getElementById('yourProcess');
+    var checkProcess = document.getElementById('checkProcess');
+    function addProcess(x){
+        var T = document.createElement('p');
+        T.innerHTML = x;
+        yourProcess.appendChild(T);
+    }
+    for(var i = 0; i < process.length; i++){
+        addProcess(process[i]);
+    }
+    var b = ["- Heat the water with stove", "- Warm tea sets with boiling water", "- Get some tea leaves", "- Brew tea in Cha Hu", "- Pour liquid into Cha Hai", "- Fill Pin Ming Bei with tea"];
+    if (process.sort().toString() == b.sort().toString()){
+        var T = document.createElement('h1');
+        T.innerHTML = 'Exactly! You are a master of tea!';
+        yourProcess.appendChild(T);
+    }else{
+        var T = document.createElement('h1');
+        T.innerHTML = 'Oops! You brew tea in a wrong way.';
+        var T1 = document.createElement('p');
+        T1.innerHTML = 'You should firstly heat the water and warm the cup. Then place tea leaves in Cha Hu and brew it. Finally you should fill the Pin Ming Bei with tea from Cha Hai.';
+        yourProcess.appendChild(T);
+        yourProcess.appendChild(T1);
     }
 }
 
