314a38ab47db6d1df4e73ec7fc0d44d1c6a5792a
LyricLiu <yichiliu@foxmail.com>
2016-09-18 11:26:00 -0400

    add Li

--- Aside-control.js
+++ Aside-control.js
@@ -4,6 +4,7 @@
 
     var process = [];
 
+
     var askForTea = document.getElementById('askForTea');
     var stove = document.getElementById('stove');
     var teaBox = document.getElementById('teaBox');
@@ -13,6 +14,10 @@
     var pinMingBei = document.getElementById('pinMingBei');
     var reward = document.getElementById('reward');
     var check = document.getElementById('check');
+
+    //Li
+    var oolong = document.getElementById('oolong');
+    var brewTea = document.getElementById('brewTea');
 
 
     var teaSetBar = document.getElementById('add');
@@ -46,144 +51,154 @@
     addItemAl('./item-pic/5.png', 'Pin Ming Bei');
     addItemAl('./item-pic/4.png', 'Stove');
     addItemAl('./item-pic/3.png', 'Cha Hai');
-   
-//stove
-function stoveAlice(){
-    stove.style.display = "block";
-    askForTea.style.display = "none";
-    chaHu.style.display = "none";
-    chaHai.style.display = "none";
-    var stove1 = document.getElementById('stove1');
-    var stove2 = document.getElementById('stove2');
-    var stove3 = document.getElementById('stove3');
-    if(getTea == false){
-      stove1.style.display = "block";
-      stove2.style.display = "none";
-      stove3.style.display = "none";
-    }else{
-        if(getChaHu == true){
-            stove2.style.display = "block";
-            stove1.style.display = "none";
-            stove3.style.display = "none";
-        }else{
-            stove3.style.display = "block";
-            stove1.style.display = "none";
+
+    //stove
+    function stoveAlice() {
+        stove.style.display = "block";
+        askForTea.style.display = "none";
+        chaHu.style.display = "none";
+        chaHai.style.display = "none";
+        var stove1 = document.getElementById('stove1');
+        var stove2 = document.getElementById('stove2');
+        var stove3 = document.getElementById('stove3');
+        if (getTea == false) {
+            stove1.style.display = "block";
             stove2.style.display = "none";
+            stove3.style.display = "none";
+        } else {
+            if (getChaHu == true) {
+                stove2.style.display = "block";
+                stove1.style.display = "none";
+                stove3.style.display = "none";
+            } else {
+                stove3.style.display = "block";
+                stove1.style.display = "none";
+                stove2.style.display = "none";
+            }
+        }
+        getStove = true;
+        process.push('- Heat the water with stove');
+    }
+
+    function teaBoxAlice() {
+        stove.style.display = "none";
+        askForTea.style.display = "none";
+        teaBox.style.display = "block";
+        warmCup.style.display = "none";
+        getTea = true;
+        process.push('- Get some tea leaves');
+    }
+
+    function chaHuAlice() {
+        stove.style.display = "none";
+        askForTea.style.display = "none";
+        teaBox.style.display = "none";
+        chaHu.style.display = "block";
+        var chaHu1 = document.getElementById('chaHu1');
+        var chaHu2 = document.getElementById('chaHu2');
+        if (getStove == false) {
+            chaHu1.style.display = "block";
+            chaHu2.style.display = "none";
+            process.push('- Add tea leaves to Cha Hu');
+        } else {
+            chaHu2.style.display = "block";
+            chaHu1.style.display = "none";
+            process.push('- Brew tea in Cha Hu');
+        }
+        getChaHu = true;
+    }
+
+    function warmCupAlice() {
+        stove.style.display = "none";
+        askForTea.style.display = "none";
+        warmCup.style.display = "block";
+        process.push('- Warm tea sets with boiling water');
+    }
+
+    function chaHaiAlice() {
+        teaBox.style.display = "none";
+        warmCup.style.display = "none";
+        chaHai.style.display = "block";
+        chaHu.style.display = "none";
+        stove.style.display = "none";
+        var chaHai1 = document.getElementById('chaHai1');
+        var chaHai2 = document.getElementById('chaHai2');
+        if (getStove == true) {
+            chaHai1.style.display = "block";
+            chaHai2.style.display = "none";
+            if (getChaHu == true) {
+                process.push('- Pour liquid into Cha Hai');
+            } else {
+                process.push('- Brew tea in Cha Hai');
+            }
+        } else {
+            chaHai2.style.display = "block";
+            chaHai1.style.display = "none";
+            process.push('- Add tea leaves to Cha Hai');
         }
     }
-    getStove = true;
-    process.push('- Heat the water with stove');
-}
 
-function teaBoxAlice(){
-    stove.style.display = "none";
-    askForTea.style.display = "none";
-    teaBox.style.display = "block";
-    warmCup.style.display = "none";
-    getTea = true;
-    process.push('- Get some tea leaves');
-}
-
-function chaHuAlice(){
-    stove.style.display = "none";
-    askForTea.style.display = "none";
-    teaBox.style.display = "none";
-    chaHu.style.display = "block";
-    var chaHu1 = document.getElementById('chaHu1');
-    var chaHu2 = document.getElementById('chaHu2');
-    if(getStove == false){
-      chaHu1.style.display = "block";
-      chaHu2.style.display = "none";
-      process.push('- Add tea leaves to Cha Hu');
-    }else{
-      chaHu2.style.display = "block";
-      chaHu1.style.display = "none";
-      process.push('- Brew tea in Cha Hu');
+    function pinMingBeiAlice() {
+        chaHai.style.display = "none";
+        chaHu.style.display = "none";
+        pinMingBei.style.display = "block";
+        process.push('- Fill Pin Ming Bei with tea');
     }
-    getChaHu = true;
-}
 
-function warmCupAlice(){
-    stove.style.display = "none";
-    askForTea.style.display = "none";
-    warmCup.style.display = "block";
-    process.push('- Warm tea sets with boiling water');
-}
-
-function chaHaiAlice(){
-    teaBox.style.display = "none";
-    warmCup.style.display = "none";
-    chaHai.style.display = "block";
-    chaHu.style.display = "none";
-    stove.style.display = "none";
-    var chaHai1 = document.getElementById('chaHai1');
-    var chaHai2 = document.getElementById('chaHai2');
-    if(getStove == true){
-      chaHai1.style.display = "block";
-      chaHai2.style.display = "none";
-      if(getChaHu == true){
-        process.push('- Pour liquid into Cha Hai');
-      }else{
-        process.push('- Brew tea in Cha Hai');
-      }
-    }else{
-      chaHai2.style.display = "block";
-      chaHai1.style.display = "none";
-      process.push('- Add tea leaves to Cha Hai');
+    function rewardAlice() {
+        reward.style.display = "block";
+        pinMingBei.style.display = "none";
     }
-}
 
-function pinMingBeiAlice(){
-    chaHai.style.display = "none";
-    chaHu.style.display = "none";
-    pinMingBei.style.display = "block";
-    process.push('- Fill Pin Ming Bei with tea');
-}
+    function checkAlice() {
+        reward.style.display = "none";
+        pinMingBei.style.display = "none";
+        check.style.display = "block";
+        var yourProcess = document.getElementById('yourProcess');
+        var checkProcess = document.getElementById('checkProcess');
 
-function rewardAlice(){
-    reward.style.display = "block";
-    pinMingBei.style.display = "none";
-}
-
-function checkAlice(){
-    reward.style.display = "none";
-    pinMingBei.style.display = "none";
-    check.style.display = "block";
-    var yourProcess = document.getElementById('yourProcess');
-    var checkProcess = document.getElementById('checkProcess');
-    function addProcess(x){
-        var T = document.createElement('p');
-        T.innerHTML = x;
-        yourProcess.appendChild(T);
+        function addProcess(x) {
+            var T = document.createElement('p');
+            T.innerHTML = x;
+            yourProcess.appendChild(T);
+        }
+        for (var i = 0; i < process.length; i++) {
+            addProcess(process[i]);
+        }
+        var b = ["- Heat the water with stove", "- Warm tea sets with boiling water", "- Get some tea leaves", "- Brew tea in Cha Hu", "- Pour liquid into Cha Hai", "- Fill Pin Ming Bei with tea"];
+        if (process.sort().toString() == b.sort().toString()) {
+            var T = document.createElement('h1');
+            T.innerHTML = 'Exactly! You are a master of tea!';
+            yourProcess.appendChild(T);
+        } else {
+            var T = document.createElement('h1');
+            T.innerHTML = 'Oops! You brew tea in a wrong way.';
+            var T1 = document.createElement('p');
+            T1.innerHTML = 'You should firstly heat the water and warm the cup. Then place tea leaves in Cha Hu and brew it. Finally you should fill the Pin Ming Bei with tea from Cha Hai.';
+            yourProcess.appendChild(T);
+            yourProcess.appendChild(T1);
+        }
     }
-    for(var i = 0; i < process.length; i++){
-        addProcess(process[i]);
+
+    function backAlice() {
+        check.style.display = "none";
+        askForTea.style.display = "block";
+        getTea = false;
+        getStove = false;
+        getChaHu = false;
+
+        process = [];
+        yourProcess.innerHTML = '';
     }
-    var b = ["- Heat the water with stove", "- Warm tea sets with boiling water", "- Get some tea leaves", "- Brew tea in Cha Hu", "- Pour liquid into Cha Hai", "- Fill Pin Ming Bei with tea"];
-    if (process.sort().toString() == b.sort().toString()){
-        var T = document.createElement('h1');
-        T.innerHTML = 'Exactly! You are a master of tea!';
-        yourProcess.appendChild(T);
-    }else{
-        var T = document.createElement('h1');
-        T.innerHTML = 'Oops! You brew tea in a wrong way.';
-        var T1 = document.createElement('p');
-        T1.innerHTML = 'You should firstly heat the water and warm the cup. Then place tea leaves in Cha Hu and brew it. Finally you should fill the Pin Ming Bei with tea from Cha Hai.';
-        yourProcess.appendChild(T);
-        yourProcess.appendChild(T1);
+
+    //Li
+     function oolongLi() {
+        askForTea.style.display = "none";
+        oolong.style.display = "block";
     }
-}
 
-function backAlice(){
-    check.style.display = "none";
-    askForTea.style.display = "block";
-    getTea = false;
-    getStove = false;
-    getChaHu = false;
-
-    process = [];
-    yourProcess.innerHTML = '';
-}
-
-
-
+     function brewTeaLi() {
+        askForTea.style.display = "none";
+        oolong.style.display = "none";
+        brewTea.style.display = "block";
+    }
